efeb3e386a463194290f7452272eef38b9f4672e
joana <yonbriones4@gimal.com>
2025-08-28 09:30:49 -0600

    dupdete

--- laboratorio 4.R
+++ laboratorio 4.R
@@ -31,3 +31,38 @@ ggplot(calidad, aes(x=IE, color=Tratamiento))+
 
 ggplot(calidad, aes(x=IE, color=Tratamiento))+
   geom_histogram()
+
+# separar los datos con tratamientos
+
+df_Ctlr <- subset(calidad, Tratamiento == "Ctrl")
+df_fert <- subset(calidad, Tratamiento != "Ctrl")
+
+# goplot revisar la 
+
+par(mfrow = c (1,2))
+qqnorm(df_Ctlr$IE);qqline(df_Ctlr$IE)
+qqnorm(df_fert$IE);qqline(df_fert$IE)
+
+
+shapiro.test(df_Ctlr$IE)
+shapiro.test(df_fert$IE)
+
+# revisar la homogeniedad 
+
+var.test(df_Ctlr$IE, df_fert$IE)
+var.test(calidad$IE ~ calidad$Tratamiento)
+
+# aplicar la prueba de t, variansas iguales 
+# dos colas = two.sided
+
+t.test(calidad$IE ~ calidad$Tratamiento,
+       var.equal = T,
+       alternative = "two.sided")
+
+cohens_efecto <- function(x, y) {
+  n1 <- length(x); n2 <- length(y)
+  s1 <- sd(x); s2 <- sd(y) 
+  sp <- sqrt(((n1 -1) * s1^2 + (n2 -1) * s2^2) / (n1 + n2 -2))
+  (mean(x) - mean(y)) / sp
+}
+d_cal <- cohens_efecto(df_Ctlr$IE, df_fert$IE)
